3e0d5847b8e83234642691be6ef154c2de0a5362
Ben Skubi <skubi@ohsu.edu>
2024-08-07 20:13:38 -0700

    Mostly completed hich compartments

--- scripts/hich/hich/__main__.py
+++ scripts/hich/hich/__main__.py
@@ -4,11 +4,29 @@ import hich.digest as _digest
 from hich.fragtag import tag_restriction_fragments
 from hich.hicrep_combos import hicrep_combos
 import hich.coverage as _coverage
+from hich.compartments import write_compartment_scores
 import polars as pl
+from pathlib import Path
 
 @click.group
 def hich():
     pass
+
+@hich.command
+@click.option("--chroms", type = StrList, default = None)
+@click.option("--exclude-chroms", type = StrList, default = None)
+@click.option("--keep-chroms-when", type = str, default = None)
+@click.option("--n_eigs", type = int, default = 1)
+@click.argument("reference")
+@click.argument("matrix")
+@click.argument("resolution", type = int)
+def compartments(chroms, exclude_chroms, keep_chroms_when, n_eigs, reference, matrix, resolution):
+    matrix = Path(matrix)
+    reference = Path(reference)
+    final_suffix = matrix.suffixes[-1]
+    prefix = matrix.name.rstrip(final_suffix)
+
+    write_compartment_scores(prefix, matrix, reference, resolution, chroms, exclude_chroms, keep_chroms_when, n_eigs)
 
 @hich.command()
 @click.option("--strata", type = IntList, default = "0")
