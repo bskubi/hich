f41c1348ac37fd11ebc34054d1b29fd72f8f0d4e
Ben Skubi <bskubi@gmail.com>
2025-08-08 08:54:14 -0700

    Update Dockerfiles, config mem limits

---
+++ env/docker/push.sh
@@ -0,0 +1,8 @@
+docker push bskubi/hich_alignment:latest
+docker push bskubi/hich_cooler:latest
+docker push bskubi/hich_hicexplorer:latest
+docker push bskubi/hich_hictk:latest
+docker push bskubi/hich_juicer:latest
+docker push bskubi/hich_multiqc:latest
+docker push bskubi/hich_mustache:latest
+docker push bskubi/hich_tools:latest
