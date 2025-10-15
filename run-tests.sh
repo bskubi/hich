8443494f4b0716857cdb4b333b6c3d0d6e8ab532
Ben Skubi <bskubi@gmail.com>
2025-10-15 15:32:04 -0700

    Remove run-tests.sh & ignore dev-run-tests.sh

--- run-tests.sh
+++
@@ -1,3 +0,0 @@
-#nf-test test --tag 3to10s --csv docs/dev/Data/nf-test-results/nf-test-3to10s.csv
-#nf-test test --tag SmokeTest_LabelMatrixPlans,SmokeTest_SelectPairs_buildCmd
-nf-test test --tag SmokeTest_LabelMatrixPlans
