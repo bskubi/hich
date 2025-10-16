764560fad31043c1402704c3f7883b94db7042f9
Ben Skubi <bskubi@gmail.com>
2025-10-15 18:39:08 -0700

    Test align with M129 dataset from Trussert 2017

--- tests/assets/fastq/interleave.py
+++ tests/assets/fastq/interleave.py
@@ -1,18 +1,32 @@
-import smart_open
+from smart_open import smart_open
 import smart_open_with_pbgzip
+import click
 
-lines1 = smart_open.smart_open("CEMBA3C_18B3C_R2_P5-2-M17-L15.R1.fq.gz", "rt").readlines()
-lines2 = smart_open.smart_open("CEMBA3C_18B3C_R2_P5-2-M17-L15.R2.fq.gz", "rt").readlines()
-count = len(lines1) + len(lines2)
-output = open("CEMBA3C_18B3C_R2_P5-2-M17-L15_interleaved.fastq", "w")
-l1 = 0
-l2 = 0
-while l1 < len(lines1) or l2 < len(lines2):
-    if l1 <= l2:
-        lines = lines1[l1:l1+4]
-        l1 += 4
-    else:
-        lines = lines2[l2:l2+4]
-        l2 += 4
-    for line in lines:
-        output.write(line)
+@click.command
+@click.argument("fq1")
+@click.argument("fq2")
+@click.argument("fq-out")
+def interleave(fq1, fq2, fq_out):
+    try:
+        lines1 = smart_open(fq1, "rt").readlines()
+        lines2 = smart_open(fq2, "rt").readlines()
+
+        count = len(lines1) + len(lines2)
+        output = smart_open(fq_out, "w")
+        l1 = 0
+        l2 = 0
+        while l1 < len(lines1) or l2 < len(lines2):
+            if l1 <= l2:
+                lines = lines1[l1:l1+4]
+                l1 += 4
+            else:
+                lines = lines2[l2:l2+4]
+                l2 += 4
+            for line in lines:
+                output.write(line)
+    except Exception as e:
+        raise Exception(f"With arguments {fq1}, {fq2}, {fq_out}") from e
+if __name__ == "__main__":
+    interleave()
+
+
